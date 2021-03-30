8425e229f7d4d836f949b5a705127335fedcdcc9
pennyman <40158855+pennyman@users.noreply.github.com>
2021-03-31 02:49:57 +0900

    sms result 처리

---
+++ result/index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var AWS = require("aws-sdk");
+var sqs = new AWS.SQS({region: 'ap-northeast-2'});
+
+var TASK_QUEUE_URL = 'https://sqs.ap-northeast-2.amazonaws.com/580729374129/dev-showprise-sms-result-sqs';
+var mysql = require('mysql');
+var phoneNo = '';
+var messageId = '';
+var status = 0;
+
+function work(task, cb) {
+  console.log(task);
+  console.log(task.Records[0].body);
+
+  // var profile = task.Records[0].body.ResponseMetadata;
+  console.log("HTTPStatusCode");
+  var profile = JSON.parse(task.Records[0].body);
+  console.log(profile);
+  console.log(profile.ResponseMetadata);
+  console.log(profile.ResponseMetadata.HTTPStatusCode);
+  messageId = profile.MessageId;
+  console.log("MessageId");
+  console.log(messageId);
+
+  if(profile.ResponseMetadata.HTTPStatusCode == 200){
+    status = 1;
+  } else {
+    status = 0;
+  }
+
+  var jsonData = JSON.stringify(task.Records[0].messageAttributes);
+  console.log(jsonData);
+  var msg = JSON.parse(jsonData);
+  console.log("phone_number");
+  console.log(msg.phone_number.stringValue);
+  phoneNo = msg.phone_number.stringValue;
+
+  console.log("ReceiptHandlePrev");
+  console.log(task.Records[0].receiptHandle);
+
+  sqs.deleteMessage({
+    ReceiptHandle: task.Records[0].receiptHandle,
+    QueueUrl: TASK_QUEUE_URL
+  });
+
+  console.log("ReceiptHandleNext");
+  cb();
+}
+
+exports.handler = function(event, context, callback) {
+  work(event, function(err) {
+    if (err) {
+      callback(err);
+    } else {
+      var connection = mysql.createConnection({
+          host     : 'dev-showprise-db.cujvftkj3jra.ap-northeast-2.rds.amazonaws.com',
+          user     : 'ses_lambda_user',
+          password : 'devShowprise2018$',
+          port     : 3306,
+          database : 'showprise'
+      });
+
+      connection.connect(function(error){
+          if(error){
+              console.log("Couldn't connect :( " + error);
+          } else {
+              console.log("Connected successfully~!");
+          }
+      });
+
+      var resultSql = "INSERT INTO sms_result (message_id, phone_no, status, reg_date) VALUES (?,?,?, now())";
+      var resultParams = [messageId, phoneNo, status];
+      connection.query(resultSql, resultParams, function(err, rows, fields){
+      	if(err){
+      		console.log(err);
+      	} else {
+      		console.log(rows.insertId);
+      	}
+      });
+
+      connection.end(function(err) {
+        // The connection is terminated now
+        if (err) {
+          console.error('[connection.end]err: ' + err);
+          connection.destroy()
+          return;
+        }
+        console.log('connection ended');
+      });
+
+    }
+  });
+};
